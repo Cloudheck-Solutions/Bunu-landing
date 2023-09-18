68d92d8fd6b7a4dba932b52c0cc2c6a519731894
Mister-Gee <fakuadegbenga@gmail.com>
2023-09-18 15:59:44 +0100

    update

--- src/Pages/Landing.jsx
+++ src/Pages/Landing.jsx
@@ -18,6 +18,67 @@ const Landing = () => {
                 <Button>Download the app</Button>
             </div>
         </div>
+        <div className="content-section">
+          <div className="title">Earn money with Bunu</div>
+          <div className="content-btn">
+            <Button className="active">Artisan</Button>
+            <Button>Client</Button>
+            <Button>Organization</Button>
+          </div>
+          <div className="content-body">
+            <img src="assets/images/image1.png" alt="img1" />
+            <div className="content-body-text">
+              <div className="content-body-text-title">Work and earn extra money</div>
+              <div className="content-body-list-wrapper">
+                <div className="content-body-text-list">
+                  <div>
+                  <div className="content-body-text-numbering">
+                    1
+                  </div>
+                  </div>
+                  <div>
+                    <div className="content-body-text-header">
+                    Work and earn extra income
+                    </div>
+                    <div className="content-body-text-body">
+                    We offer the best chance for artisans to find customers on our mobile application with over 10+ million users. 
+                    </div>
+                  </div>
+                </div>
+                <div className="content-body-text-list">
+                  <div>
+                  <div className="content-body-text-numbering">
+                    2
+                  </div>
+                  </div>
+                  <div>
+                    <div className="content-body-text-header">
+                    Get Paid weekly
+                    </div>
+                    <div className="content-body-text-body">
+                    We offer the best chance for artisans to find customers on our.
+                    </div>
+                  </div>
+                </div>
+                <div className="content-body-text-list">
+                <div>
+                <div className="content-body-text-numbering">
+                  3
+                </div>
+                </div>
+                <div>
+                  <div className="content-body-text-header">
+                  Work and earn extra income
+                  </div>
+                  <div className="content-body-text-body">
+                  We offer the best chance for artisans to find customers on our mobile application with.
+                  </div>
+                </div>
+              </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )

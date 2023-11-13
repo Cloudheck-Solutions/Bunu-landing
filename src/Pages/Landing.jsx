6e90c52c2764e25ec1b8fd2513c1676e5332d640
Mister-Gee <fakuadegbenga@gmail.com>
2023-11-13 02:54:56 +0100

    update

--- src/Pages/Landing.jsx
+++ src/Pages/Landing.jsx
@@ -1,176 +1,237 @@
-import React from 'react'
-import Header from '../Components/Header'
-import { Button } from 'react-bootstrap'
-import { Icon } from '@iconify/react'
-import Footer from '../Components/Footer'
+import React from "react";
+import Header from "../Components/Header";
+import { Button } from "react-bootstrap";
+import { Icon } from "@iconify/react";
+import Footer from "../Components/Footer";
 
 const Landing = () => {
   return (
-    <div className='landing-wrapper'>
+    <div className="landing-wrapper">
+      <Header />
       <div className="hero-section">
-        <Header />
-        <div className="hero-content">
-            <div className="hero-content-title">
-                Find Artisans near you
-            </div>
+        <div className="hero-section2">
+          <div className="hero-content">
+            <div className="hero-content-title">Find Artisans near you</div>
             <div className="hero-content-sub-title">
-            Pick the best artisans in the country to fix your basic household needs
+              Pick the best artisans in the country to fix your basic household
+              needs
             </div>
             <div className="hero-content-download-btn">
-                <Button>Download the app</Button>
+              <Button>Download the app</Button>
             </div>
+          </div>
         </div>
       </div>
       <div className="content-section">
-          <div className="title">Earn money with Bunu</div>
-          <div className="content-btn">
-            <Button className="active">Artisan</Button>
-            <Button>Client</Button>
-            <Button>Organization</Button>
+        <div className="title">About Us</div>
+        <div className="content-body">
+          <div className="content-body-text">
+            <div className="contentAboutus">
+              <div className="contentAboutUsText">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad veniam, quis nostrud exercitation ullamco laboris nisi
+                ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+                repreh
+              </div>
+              <div className="abt-btn-section">
+                <Button className="abt-btn">READ MORE</Button>
+              </div>
+            </div>
           </div>
-          <div className="content-body">
-            <img src="assets/images/image1.png" alt="img1" />
-            <div className="content-body-text">
-              <div className="content-body-text-title">Work and earn extra money</div>
-              <div className="content-body-list-wrapper">
-                <div className="content-body-text-list">
-                  <div>
-                  <div className="content-body-text-numbering">
-                    1
-                  </div>
-                  </div>
-                  <div>
-                    <div className="content-body-text-header">
-                    Work and earn extra income
-                    </div>
-                    <div className="content-body-text-body">
-                    We offer the best chance for artisans to find customers on our mobile application with over 10+ million users. 
-                    </div>
-                  </div>
-                </div>
-                <div className="content-body-text-list">
-                  <div>
-                  <div className="content-body-text-numbering">
-                    2
-                  </div>
-                  </div>
-                  <div>
-                    <div className="content-body-text-header">
-                    Get Paid weekly
-                    </div>
-                    <div className="content-body-text-body">
-                    We offer the best chance for artisans to find customers on our.
-                    </div>
-                  </div>
-                </div>
-                <div className="content-body-text-list">
+          <img src="assets/images/aboutus.png" alt="img1" />
+        </div>
+      </div>
+      <div className="content-section">
+        <div className="title">Earn money with Bunu</div>
+        <div className="content-btn">
+          <Button className="active">Artisan</Button>
+          <Button>Client</Button>
+          <Button>Organization</Button>
+        </div>
+        <div className="content-body">
+          <img src="assets/images/image1.png" alt="img1" />
+          <div className="content-body-text">
+            <div className="content-body-text-title">
+              Work and earn extra money
+            </div>
+            <div className="content-body-list-wrapper">
+              <div className="content-body-text-list">
                 <div>
-                <div className="content-body-text-numbering">
-                  3
-                </div>
+                  <div className="content-body-text-numbering">1</div>
                 </div>
                 <div>
                   <div className="content-body-text-header">
-                  Work and earn extra income
+                    Work and earn extra income
                   </div>
                   <div className="content-body-text-body">
-                  We offer the best chance for artisans to find customers on our mobile application with.
+                    We offer the best chance for artisans to find customers on
+                    our mobile application with over 10+ million users.
                   </div>
                 </div>
               </div>
+              <div className="content-body-text-list">
+                <div>
+                  <div className="content-body-text-numbering">2</div>
+                </div>
+                <div>
+                  <div className="content-body-text-header">
+                    Get Paid weekly
+                  </div>
+                  <div className="content-body-text-body">
+                    We offer the best chance for artisans to find customers on
+                    our.
+                  </div>
+                </div>
+              </div>
+              <div className="content-body-text-list">
+                <div>
+                  <div className="content-body-text-numbering">3</div>
+                </div>
+                <div>
+                  <div className="content-body-text-header">
+                    Work and earn extra income
+                  </div>
+                  <div className="content-body-text-body">
+                    We offer the best chance for artisans to find customers on
+                    our mobile application with.
+                  </div>
+                </div>
               </div>
             </div>
           </div>
         </div>
-        <div className="info-section">
-          <div className="info-section-item">
-            <div className="info-item-header">
-              <div className="info-icon"> <Icon icon="formkit:people" /> </div>
-              <div className="title">About Us</div>
+      </div>
+      <div className="info-section">
+        <div className="info-section-item">
+          <div className="info-item-header">
+            <div className="info-icon">
+              {" "}
+              <Icon icon="formkit:people" />{" "}
             </div>
-            <div className="info-item-body">Find out how we started, what drives us, and how we're reimagining how the world moves.</div>
+            <div className="title">About Us</div>
           </div>
-          <div className="info-section-item">
-            <div className="info-item-header">
-              <div className="info-icon"> <Icon icon="iconamoon:news" /> </div>
-              <div className="title">Newsroom</div>
-            </div>
-            <div className="info-item-body">See announcements about our latest releases, initiatives, and partnerships.</div>
-          </div>
-          <div className="info-section-item">
-            <div className="info-item-header">
-              <div className="info-icon"> <Icon icon="mi:call" /> </div>
-              <div className="title">Call Us</div>
-            </div>
-            <div className="info-item-body">Call us anytime and request for help with our 24/7 support at +234 90 593 2342 423.</div>
+          <div className="info-item-body">
+            Find out how we started, what drives us, and how we're reimagining
+            how the world moves.
           </div>
         </div>
-        <div className="app-info">
-          <div className="title">It’s easier with our app</div>
-          <div className="app-info-content-wrapper">
-            <div className="app-info-details">
-              <div className="detail-header">Request for artisans, for <br/> quality work.</div>
-              <div className="detail-body">Scan the Qr code with your phone camera to download the Bunu app. Available for Android and IOS devices.</div>
-              <a href="/#">Click here to download</a>
+        <div className="info-section-item">
+          <div className="info-item-header">
+            <div className="info-icon">
+              {" "}
+              <Icon icon="iconamoon:news" />{" "}
             </div>
-            <div className="demo-image-wrapper">
-              <img src='assets/images/phone.png' alt='bunu app demo' />
-            </div>
+            <div className="title">Newsroom</div>
+          </div>
+          <div className="info-item-body">
+            See announcements about our latest releases, initiatives, and
+            partnerships.
           </div>
         </div>
-        <div className="pre-footer">
-          <div className="logo-social">
-            <div className="footer-logo">
-              <div className="footer-logo-image">
-                <img src='assets/images/logo2.png' alt='bunu' />
-              </div>
-              <div className="footer-logo-text">Bunu</div>
+        <div className="info-section-item">
+          <div className="info-item-header">
+            <div className="info-icon">
+              {" "}
+              <Icon icon="mi:call" />{" "}
             </div>
-            <div className="footer-social">
-              <a href="/#" className="footer-social-wrapper">
-                <Icon icon="ri:instagram-fill" /> 
-              </a>
-              <a href="/#" className="footer-social-wrapper">
-                <Icon icon="ri:linkedin-fill" /> 
-              </a>
-              <a href="/#" className="footer-social-wrapper">
-                <Icon icon="ri:whatsapp-fill" /> 
-              </a>
-              <a href="/#" className="footer-social-wrapper">
-                <Icon icon="bi:facebook" /> 
-              </a>
-              <a href="/#" className="footer-social-wrapper">
-                <Icon icon="uil:twitter" /> 
-              </a>
-            </div>
+            <div className="title">Call Us</div>
           </div>
-          <div className="footer-links">
-            <div className="link-wrapper">
-              <div className="title">Quicklinks</div>
-              <a href="#/" className="footer-link">Home</a>
-              <a href="#/" className="footer-link">About Us</a>
-            </div>
-            <div className="link-wrapper">
-              <div className="title">Company</div>
-              <a href="#/" className="footer-link">Careers</a>
-              <a href="#/" className="footer-link">Newsroom</a>
-              <a href="#/" className="footer-link">Careers</a>
-              <a href="#/" className="footer-link">Newsroom</a>
-            </div>
-            <div className="link-wrapper">
-              <div className="title">Download</div>
-              <a href="#/" className="footer-link2">
-                <img src='assets/images/app-store.png' alt='Apple App store download' />
-              </a>
-              <a href="#/" className="footer-link2">
-                <img src='assets/images/play.png' alt='Google Play store download' />
-              </a>
-            </div>
+          <div className="info-item-body">
+            Call us anytime and request for help with our 24/7 support at +234
+            90 593 2342 423.
           </div>
         </div>
-        <Footer />
+      </div>
+      <div className="app-info">
+        <div className="title">It’s easier with our app</div>
+        <div className="app-info-content-wrapper">
+          <div className="app-info-details">
+            <div className="detail-header">
+              Request for artisans, for <br /> quality work.
+            </div>
+            <div className="detail-body">
+              Scan the Qr code with your phone camera to download the Bunu app.
+              Available for Android and IOS devices.
+            </div>
+            <a href="/#">Click here to download</a>
+          </div>
+          <div className="demo-image-wrapper">
+            <img src="assets/images/phone.png" alt="bunu app demo" />
+          </div>
+        </div>
+      </div>
+      <div className="pre-footer">
+        <div className="logo-social">
+          <div className="footer-logo">
+            <div className="footer-logo-image">
+              <img src="assets/images/logo2.png" alt="bunu" />
+            </div>
+            <div className="footer-logo-text">Bunu</div>
+          </div>
+          <div className="footer-social">
+            <a href="/#" className="footer-social-wrapper">
+              <Icon icon="ri:instagram-fill" />
+            </a>
+            <a href="/#" className="footer-social-wrapper">
+              <Icon icon="ri:linkedin-fill" />
+            </a>
+            <a href="/#" className="footer-social-wrapper">
+              <Icon icon="ri:whatsapp-fill" />
+            </a>
+            <a href="/#" className="footer-social-wrapper">
+              <Icon icon="bi:facebook" />
+            </a>
+            <a href="/#" className="footer-social-wrapper">
+              <Icon icon="uil:twitter" />
+            </a>
+          </div>
+        </div>
+        <div className="footer-links">
+          <div className="link-wrapper">
+            <div className="title">Quicklinks</div>
+            <a href="#/" className="footer-link">
+              Home
+            </a>
+            <a href="#/" className="footer-link">
+              About Us
+            </a>
+          </div>
+          <div className="link-wrapper">
+            <div className="title">Company</div>
+            <a href="#/" className="footer-link">
+              Careers
+            </a>
+            <a href="#/" className="footer-link">
+              Newsroom
+            </a>
+            <a href="#/" className="footer-link">
+              Careers
+            </a>
+            <a href="#/" className="footer-link">
+              Newsroom
+            </a>
+          </div>
+          <div className="link-wrapper">
+            <div className="title">Download</div>
+            <a href="#/" className="footer-link2">
+              <img
+                src="assets/images/app-store.png"
+                alt="Apple App store download"
+              />
+            </a>
+            <a href="#/" className="footer-link2">
+              <img
+                src="assets/images/play.png"
+                alt="Google Play store download"
+              />
+            </a>
+          </div>
+        </div>
+      </div>
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Landing
+export default Landing;
